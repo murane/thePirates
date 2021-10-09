1f2bf372ee5ccd2d2a2da2c683f6e9025864080d
jun <murane@naver.com>
2021-10-10 05:12:25 +0900

    feat: CreateProductOption service
    CreateProductOption 서비스 메소드 추가

---
+++ src/main/java/com/example/thepirates/service/dto/ProductOptionService.java
@@ -0,0 +1,39 @@
+package com.example.thepirates.service.dto;
+
+import com.example.thepirates.api.dto.request.ProductOptionInfo;
+import com.example.thepirates.domain.Product;
+import com.example.thepirates.domain.ProductOption;
+import com.example.thepirates.domain.ProductOptionRepository;
+import com.example.thepirates.service.dto.input.ProductOptionCreateInput;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductOptionService {
+    private final ProductOptionRepository productOptionRepository;
+
+    @Transactional
+    public List<ProductOption> createProductOptions(ProductOptionCreateInput input) {
+        return input.getOptionInfos().stream()
+                .map(
+                        it -> createProductOption(it, input.getProduct())
+                )
+                .collect(Collectors.toList());
+    }
+
+    private ProductOption createProductOption(ProductOptionInfo productOptionInfo, Product product) {
+        return productOptionRepository.save(
+                ProductOption.builder()
+                        .product(product)
+                        .name(productOptionInfo.getName())
+                        .stock(productOptionInfo.getStock())
+                        .price(productOptionInfo.getPrice())
+                        .build()
+        );
+    }
+}
---
+++ src/main/java/com/example/thepirates/service/dto/input/ProductOptionCreateInput.java
@@ -0,0 +1,21 @@
+package com.example.thepirates.service.dto.input;
+
+import com.example.thepirates.api.dto.request.ProductOptionInfo;
+import com.example.thepirates.domain.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductOptionCreateInput {
+    private Product product;
+    private List<ProductOptionInfo> optionInfos;
+
+    public ProductOptionCreateInput(Product product, List<ProductOptionInfo> optionInfos) {
+        this.product = product;
+        this.optionInfos = optionInfos;
+    }
+}
